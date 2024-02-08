45a12b881ad4e86f6d0521fd4fbe578132c7af4a
Justina <justcho5@gmail.com>
2024-02-08 15:48:32 -0800

    fix: increase limit for request body that api will handle to 1mb fix: once recipe is updated search params should be updated to show updated recipe

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,7 +27,7 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(requestLogger);
-app.use(express.json()); // It parses incoming requests with JSON payloads and is based on body-parser
+app.use(express.json({ limit: "1mb" })); // It parses incoming requests with JSON payloads and is based on body-parser
 
 // app routing
 app.use("/api/recipes", recipesRouter);
