65570cba92db6e92ceb04c1c53fe8cb8d3b1efb9
Justina <justcho5@gmail.com>
2024-02-09 11:43:09 -0800

    feat:import by url button

--- frontend/src/pages/CreateRecipe.tsx
+++ frontend/src/pages/CreateRecipe.tsx
@@ -1,4 +1,14 @@
+import Button from "../components/Button";
 import Form from "../components/Form";
 export default function CreateRecipe() {
-  return <Form />;
+  const handleClick = () => {
+    return;
+  };
+  return (
+    <div className="flex flex-col gap-5">
+      <Button text={"Import by URL"} onClick={handleClick} />
+      <p className="text-center">-OR-</p>
+      <Form />
+    </div>
+  );
 }
