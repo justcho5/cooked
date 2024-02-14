22b5d160c078b72277aba0b73a5e34e2b65fa91b
Justina <justcho5@gmail.com>
2024-02-14 12:01:24 -0500

    removed CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 // This is my express app
 import express from "express";
 import mongoose from "mongoose";
-// import cors from "cors";
 
 import { MONGODB_URI } from "./utils/config";
 import logger from "./utils/logger";
@@ -25,7 +24,6 @@ mongoose
 
 // make an express app
 const app = express();
-// app.use(cors());
 app.use(requestLogger);
 app.use(express.json({ limit: "1mb" })); // It parses incoming requests with JSON payloads and is based on body-parser
 
